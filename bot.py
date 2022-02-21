a0edc178b78ea3e31bb2fbe8306afd1be14578fd
Gowthamr221 <gowthamr221@gmail.com>
2022-02-21 23:46:07 +0530

    feb 21 commit 2

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 from urllib import response
 import random
+import requests
+import json
 import discord
 from dotenv import load_dotenv
 
@@ -8,6 +10,12 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
 
 @client.event
 async def on_ready():
@@ -20,6 +28,10 @@ async def on_message(message):
     
     if(message.content == '!random'):
         response = random.randint(0,100)
+        await message.channel.send(response)
+    
+    if(message.content == '!quote'):
+        response = get_quote()
         await message.channel.send(response)
 
 
